96dd260083b11a9614f1e788a5321faab787dbc1
shaiknagoorvali10803 <nagoorvl@gmail.com>
2022-06-06 17:36:33 +0530

    master commit2

--- src/Demo/Print1Method.java
+++ src/Demo/Print1Method.java
@@ -3,5 +3,6 @@ package Demo;
 public class Print1Method {
     public static void main(String[] args) {
         System.out.println("main print1 method");
+        System.out.println("main print2 method");
     }
 }
