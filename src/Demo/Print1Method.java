0b5a28d0adec44cf2ca99439d8e51722b32376cf
shaiknagoorvali10803 <nagoorvl@gmail.com>
2022-06-06 17:35:36 +0530

    nagoor rebase master

--- src/Demo/Print1Method.java
+++ src/Demo/Print1Method.java
@@ -4,6 +4,7 @@ public class Print1Method {
     public static void main(String[] args) {
         System.out.println("main print1 method");
         System.out.println("nagoor print1 method");
+        System.out.println("nagoor print2 method");
         System.out.println("main print2 method");
         System.out.println("main print3 method");
     }
